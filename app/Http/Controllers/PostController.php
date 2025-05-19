92e65f43a29685e15c144d7075fbf7f4df44833f
taka3ura <taka142434@gmail.com>
2025-05-19 10:44:18 +0000

    Implement validation for post creation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(3)]);
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
 
     public function show(Post $post)
@@ -23,7 +23,7 @@ class PostController extends Controller
         return view('posts.create');
     }
 
-    public function store(Request $request, Post $post)
+    public function store(PostRequest $request, Post $post)
     {
         $input = $request['post'];
         $post->fill($input)->save();
